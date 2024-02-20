eaa0318404ce10c4e188d6ce11690bcedad9bdf4
wonseok-han <oshan1112@gmail.com>
2024-02-20 16:45:08 +0900

    Loading 컴포넌트 추가

---
+++ src/components/Loading.tsx
@@ -0,0 +1,17 @@
+import type { PropsWithChildren } from 'react';
+
+import '@assets/css/components/loading.scss';
+
+interface LoadingProps {
+  isLoading: boolean;
+}
+
+const Loading = ({ children, isLoading }: PropsWithChildren<LoadingProps>) => {
+  return (
+    <div className={`loading_backdrop ${isLoading ? 'active' : ''}`}>
+      <div className="loading_bar">{children}</div>
+    </div>
+  );
+};
+
+export default Loading;
