e67584736611872841d00a1a0dfff4693619f4b7
wonseok-han <oshan1112@gmail.com>
2024-03-18 17:19:37 +0900

    vite-env.d.ts는 lint룰 무시하도록 수정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,9 @@
 /// <reference types="vite/client" />
 
-type ImportMetaEnv = {
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+interface ImportMetaEnv {
   readonly VITE_PROJECT_ID: string;
   readonly VITE_API_BASE_URL: string;
   readonly VITE_API_POSTFIX_URL: string;
   readonly VITE_RENDER_INDICATOR_USE: 'on' | 'off';
-};
+}
