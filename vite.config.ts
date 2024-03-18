4ec41e438e46989e0d66885aacc050980db90b15
wonseok-han <oshan1112@gmail.com>
2024-03-18 18:34:24 +0900

    robots 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import directoryExporter from './plugins/directory-exporter';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +15,24 @@ export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: isProduction ? 'robots.prod.txt' : 'robots.dev.txt',
+          dest: '',
+          rename: 'robots.txt',
+        },
+      ],
+    }),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          metaRobots: isProduction
+            ? ''
+            : '<meta name="robots" content="noindex,nofollow"/>',
+        },
+      },
+    }),
     directoryExporter([
       {
         directoryPath: 'src/assets/img/*',
